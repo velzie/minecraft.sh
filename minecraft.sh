d7332535f769ab421dcec0349bf8140d815992d9
CoolElectronics <me@coolelectronics.me>
2023-09-01 21:17:51 -0400

    a bunch of new packets and temporary data dirs

--- minecraft.sh
+++ minecraft.sh
@@ -1,182 +1,247 @@
+# shellcheck shell=bash
 . ./types.sh
+
+# consts
+HOST=localhost
 PORT=25565
+TEMP=/dev/shm/minecraft.sh
+USERNAME=sh
+VERSION=763 # 1.20.1, the latest version at the time of making this. be warned, packet ids and format can change drastically between versions
+
+# 0: handshaking
+# 1: pinging
+# 2: login
+# 3: play
+#
+# note: do not trust this unless you know what thread you're on
+STATE=0
+
 
 hexpacket_len() {
-	2varint $((($(echo -n "$1" | unhex | wc -c)+1)))
-}
-send_packet(){
-  echo -n "$(hexpacket_len "$2")$1$2" | unhex >&3
-}
-# read from stdin(bytes)
-readhex(){
-	head -c $1 | hex
+	tovarint $((($(echo -n "$1" | fromhex | wc -c) + 1)))
 }
 
+# (packet_num: hex(2), data: hex(unsized))
+send_packet() {
+	echo -n "$(hexpacket_len "$2")$1$2" | fromhex >&3
+}
+# (bytes:int) -> hex string
+readhex() {
+	head -c $1 | tohex
+}
 
-login(){
+login() {
+
+	if ! [ -d "$TEMP" ]; then
+		mkdir "$TEMP"
+	fi
 
 	# ensure we're the only one
 	lsof -t lsof | xargs kill
 	exec 4<>lsof
-	exec 3<>/dev/tcp/localhost/$PORT
 
+	# we need to create temporary files in memory because bash cannot send between subshells
+	PLAYER_ID=$RANDOM
+	PIPE="$TEMP/$PLAYER_ID.pipe"
+	PLAYER="$TEMP/$USERNAME:$PLAYER_ID.mem"
+	mkfifo $PIPE
+	mkdir -p "$PLAYER"
+
+	exec 3<>/dev/tcp/$HOST/$PORT
 
 	STATE=0
-	send_packet 00 "$(2varint 763)$(2string localhost)$(2short $PORT)$(2varint 2)"
-	send_packet 00 "$(2string sh)00"
-
-# 	sleep 2
-#
-# 	# while true; do
-# 	# sleep 1
-# # done
-
-	listen<&3&
-	
-}
-
-listen(){
+	send_packet 00 "$(tovarint $VERSION)$(tostring $HOST)$(toshort $PORT)$(tovarint 2)"
 	STATE=2
-	# 0: handshaking
-	# 1: pinging
-	# 2: login
-	# 3: play	
-	
+	send_packet 00 "$(tostring $USERNAME)00"
 
-	if [[ ! -p tcp ]]; then
-		mkfifo tcp
-	fi
+	listen <&3 &
+	LISTENER_PID=$!
 
-while true; do
-
-	len=$(varint2)
-	
-
-	head -c $len<&3>tcp&
-	proc_pkt<tcp
-
-
-	# if ! head -c $len | proc_pkt; then
-	# 	echo "disconnected"
-	# 	return
-	# fi
-#
-# 	a=$(dd count=$len bs=1 status=none | hex)
-#
-# 	if [[ "$a" == '' ]]; then
-# 		echo "guh"
-# 		return
-# 	fi
-#
-#
-# 	if [[ $a == "02"* ]]; then
-# 		echo "logged in"
-# 		uuid=${a:0:32}
-# 		echo "i am $uuid"
-# 		name=$(echo ${a:32:48}|unhex)
-# 		echo "am $name"
-# 	fi
-# 	if [[ $a == "23"* ]]; then
-# 		id=${a:2}
-# 		echo "keepalive $id"
-# 		send_packet 12 "$id">&3
-# 	elif [[ $a == "35"* ]]; then
-# 		echo "chat!"
-# 		uuid=${a:0:32}
-# 		echo $uuid
-# 	fi
-done
 }
 
-proc_pkt(){
-	pkt_id=$(readhex 1)
+
+cleanup(){
+	kill $LISTENER_PID
+	exec 3>&-
+	exec 3<&-
+	if [ ! -z "$PLAYER" ] && [ -d "$PLAYER" ]; then
+		rm -rf "$PLAYER"
+	fi
+	rm -f $PIPE
+}
+
+listen() {
+
+	while true; do
+
+		# get packet length
+		len=$(fromvarint)
+
+		# motivation for using an FIFO:
+		# - subshells CANNOT be used because of the need to persist state between packet frames
+		# - here-strings CANNOT be used because of nul bytes (it's slower anyway probably
+		# - temporary files are too slow, we need to respond to keepalives as soon as possible to avoid a kick
+		# - there's no reason cmd <(subshell) wouldn't work, but it doesn't and i don't know why
+
+		head -c $len <&3 >$PIPE &
+		proc_pkt <$PIPE
+
+	done
+}
+
+proc_pkt() {
+	local pkt_id=$(readhex 1)
 	if [ "$pkt_id" == "" ]; then
-		echo "exitng"
+		echo "---- disconnected from server ----"
+		cleanup
 		exit
-		return 1
 	fi
 
 	case $STATE in
-		0) ;;
-		1) ;;
-		2) 
-			case $pkt_id in
-				00)
-					echo "Failed to login"
-					# print the disconnect reason
-					cat
-					return 1
-				;;
-				02)
-
-					uuid=$(readhex 16)
-					len=$(varint2)
-					read -n$len username
-					echo "logged in as $username"
-					STATE=3
-					pkt_respawn
-				;;
-			esac
+	0) ;;
+	1) ;;
+	2)
+		case $pkt_id in
+		00) # disconnect
+			echo "Failed to login"
+			# print the disconnect reason
+			cat
+			return 1
 			;;
-		3)
-			case $pkt_id in
-				23)
-					id=$(readhex 9999)
-					echo "keepalive packet $id"
-					send_packet 12 "$id"
-				;;
-				35)
-					uuid=$(readhex 16)
-					index=$(varint2) # unknown what this does
-					head -c 1 >/dev/null # eat the signature bool 
-					len=$(varint2)
-					message=$(head -c $len)
-					timestamp=$(readhex 8)
-					salt=$(readhex 8) # crypto related? idk
-					unknown=$(readhex 6) # no idea what this is
+		02) # login sucessful
+			uuid=$(readhex 16)
+			len=$(fromvarint)
+			read -r "-n$len" username
+			echo "logged in as $username"
+			STATE=3
+			pkt_respawn
+			;;
+		esac
+		;;
+	3)
+		case $pkt_id in
+		1a) # disconnect
+			local len=$(fromvarint)
+			echo "kicked from server: $(cat)"
+			;;
+		23) # keepalive
+			id=$(readhex 9999)
+			send_packet 12 "$id"
+			;;
 
-					username=$(jq -r ".insertion") # i don't technically *need* jq but it's easy
+		45) # server data
+			local len=$(fromvarint)
+			local motd=$(head -c$len)
 
+			echo "MOTD: $motd"
+			;;
+		3a) # player info (tab)
+			# i'll leave this unimplemented it's annoying
+			;;
+		35) # player chat
+			local uuid=$(readhex 16)
+			local index=$(fromvarint) # unknown what this does
+			head -c1 >/dev/null      # eat the signature bool
+			local len=$(fromvarint)
+			local message=$(head -c$len)
+			local timestamp=$(readhex 8)
+			local salt=$(readhex 8)    # crypto related? idk
+			local unknown=$(readhex 6) # no idea what this is
 
-					echo "<$username> $message"
-				;;
-				38)
-					id=$(varint2)
-					len=$(varint2)
-					reason=$(jq -r ".translate")
-					echo "died! $reason"
-					pkt_respawn
-				;;
-				*)
-					# echo "unknown packet $pkt_id"
-				;;
-			esac
+			local username=$(jq -r ".insertion") # i don't technically *need* jq but it's easy
+
+			echo "<$username> $message"
+			;;
+		1b) # disguised chat message. not really sure when this is used?
+			local len=$(fromvarint)
+			local message=$(head -c$len)
+			len=$(fromvarint)
+			local typename=$(head -c$len)
+			local hasname=$(readhex 1)
+			len=$(fromvarint)
+			local name=$(head -c$len)
+			echo "<system>-$message-$typename-$hasname-$name"
+			;;
+		38) # combat death
+			local id=$(fromvarint)
+			local len=$(fromvarint)
+			local reason=$(jq -r ".translate")
+			echo "died! $reason"
+			pkt_respawn
+			;;
+		57) # set health
+			local health=$(fromfloat $(readhex 4))
+			local food=$(fromvarint)
+			local saturation=$(fromfloat $(readhex 4))
+			echo "health: $health, food: $food, saturation: $saturation"
+			;;
+		28) # login
+			# get own entity-id. there are a bunch of other fields but i don't care about any of them
+			echo -n $(( 0x$(readhex 4) )) >"$PLAYER/eid"
+			echo -n 0 >"$PLAYER/seqid"
+			;;
+		*)
+			# echo "unknown packet $pkt_id"
+			;;
+		esac
 		;;
 	esac
-	cat>/dev/null
+
+	# purge all remaining data to stay in sync
+	cat >/dev/null
 }
 
-
-pkt_respawn(){
-	send_packet 07 "$(2varint 0)"
+pkt_respawn() {
+	send_packet 07 "$(tovarint 0)"
 }
 
-
-pkt_chat(){
-	pkt=$(2string $1) # message
-	pkt+=$(2long $(date +%s)) # timestamp
-	pkt+=$(2long $(date +%s)) # salt
-	pkt+="00" # has signature, bool(false)
-	pkt+="$(2varint 1)" # message count? idk what this means
-	pkt+="$(repeat 11 '00')" # "acknowleged"?? no idea what this is either but if i spam exactly 11 zeros it seems to work
+pkt_chat() {
+	local pkt=$(tostring $1)   # message
+	pkt+=$(tolong $(date +%s)) # timestamp
+	pkt+=$(tolong $(date +%s)) # salt
+	pkt+="00"                  # has signature, bool(false)
+	pkt+="$(tovarint 1)"       # message count? idk what this means
+	pkt+="$(repeat 11 '00')"   # "acknowleged"?? no idea what this is either but if i spam exactly 11 zeros it seems to work
 	send_packet 05 "$pkt"
 }
-pkt_chat_command(){
-	pkt=$(2string $1) # message
-	pkt+=$(2long $(date +%s)) # timestamp
-	pkt+=$(2long $(date +%s)) # salt
-	pkt+=$(2varint 0) # idk some crypto bullshit
-	pkt+=$(2varint 1) # message count
-	pkt+="$(repeat 11 '00')" # "acknowleged"
+pkt_chat_command() {
+	local pkt=$(tostring "$1")   # message
+	pkt+=$(tolong $(date +%s)) # timestamp
+	pkt+=$(tolong $(date +%s)) # salt
+	pkt+=$(tovarint 0)         # idk some crypto bullshit
+	pkt+=$(tovarint 1)         # message count
+	pkt+="$(repeat 11 '00')"   # "acknowleged"
 	send_packet 04 "$pkt"
+}
+
+get_seqid(){
+	SEQ_ID=$(<$PLAYER/seqid)
+}
+incrm_seqid(){
+	echo -n $(( SEQ_ID + 1))>"$PLAYER/seqid"
+}
+
+# (arm: 0 | 1)
+pkt_swing_arm(){
+	send_packet 2f $(tovarint $1)
+}
+pkt_drop_item(){
+	get_seqid
+	send_packet 1d "$(tovarint 4)$(encode_position 0 0 0)$(tovarint 0)$(tovarint $SEQ_ID)"
+	incrm_seqid
+}
+pkt_drop_stack(){
+	get_seqid
+	send_packet 1d "$(tovarint 3)$(encode_position 0 0 0)$(tovarint 0)$(tovarint $SEQ_ID)"
+	incrm_seqid
+}
+pkt_sneak(){
+	get_seqid
+	send_packet 1e "$(tovarint $(<$PLAYER/eid))$(tovarint 0)$(tovarint $SEQ_ID)"
+	incrm_seqid
+}
+pkt_unsneak(){
+	get_seqid
+	send_packet 1e "$(tovarint $(<$PLAYER/eid))$(tovarint 1)$(tovarint $SEQ_ID)"
+	incrm_seqid
 }
--- types.sh
+++ types.sh
@@ -1,4 +1,6 @@
-2varint() {
+# shellcheck shell=bash
+
+tovarint() {
 	local a
 	local b
 	local c
@@ -7,37 +9,37 @@
 	if [[ $1 -lt 128 ]]; then
 		:
 	elif [[ $1 -lt 16384 ]]; then
-		a=$(($1%128))
-		b=$(($1/128))
-		out=$(printf "%02x" $((a+128)))$(printf "%02x" $b)
-	elif [[ $1 -lt $((128*128*128)) ]]; then
-		a=$(($1%128))
-		c=$((($1/128)%128))
-		b=$(($1/16384))
-		out=$(printf "%02x" $((a+128)))$(printf "%02x" $((c+128)))$(printf "%02x" $b)
+		a=$(($1 % 128))
+		b=$(($1 / 128))
+		out=$(printf "%02x" $((a + 128)))$(printf "%02x" $b)
+	elif [[ $1 -lt $((128 * 128 * 128)) ]]; then
+		a=$(($1 % 128))
+		c=$((($1 / 128) % 128))
+		b=$(($1 / 16384))
+		out=$(printf "%02x" $((a + 128)))$(printf "%02x" $((c + 128)))$(printf "%02x" $b)
 	fi
 	echo -n "$out"
 }
 
-varint2() {
+fromvarint() {
 	local x
 	local uwu
 	local out
 	out=""
-	x=1	
+	x=1
 	while true; do
 		uwu=$(dd count=1 bs=1 status=none | xxd -p)
-	
-		out=$((out+((0x$uwu&127)*x)))
-		x=$((x*128))
-		if [[ $((0x$uwu>>7)) == 0 ]]; then
+
+		out=$((out + ((0x$uwu & 127) * x)))
+		x=$((x * 128))
+		if [[ $((0x$uwu >> 7)) == 0 ]]; then
 			break
 		fi
 	done
 	echo -n "$out"
 }
-2string(){
-	echo -n "$(2varint $(expr length $1))$(echo -n $1 | hex)"
+tostring() {
+	echo -n "$(tovarint $(expr length "$1"))$(printf "%s" "$1" | tohex)"
 }
 
 
@@ -45,17 +47,50 @@ repeat() {
 	printf -- "$2%.0s" $(seq 1 $1)
 }
 
-unhex() {
+fromhex() {
 	xxd -p -r -c999999
 }
-hex(){
-  xxd -p
+tohex() {
+	xxd -p
 }
 
-
-2short(){
+toshort() {
 	printf "%04x" $1
 }
-2long(){
+tolong() {
 	printf "%08x" $1
+}
+hex2bin() {
+	# \o/
+	echo -n "$1" | sed -E 's/0/0000/g;s/1/0001/g;s/2/0010/g;s/3/0011/g;s/4/0100/g;s/5/0101/g;s/6/0110/g;s/7/0111/g;s/8/1000/g;s/9/1001/g;s/a/1010/g;s/b/1011/g;s/c/1100/g;s/d/1101/g;s/e/1110/g;s/f/1111/g'
+}
+fromfloat() {
+	# [todo] don't use gdb as a fucking calculator
+	gdb --batch -ex "print/f (float *) 0x$1" | tail -c+6
+}
+
+decode_position() {
+	x=$((0x$1 >> 38))
+	y=$((0x$1 & 0xFFF))
+	z=$(((0x$1 >> 12) & 0x3FFFFFF))
+	
+	[[ $x -gt 33554431 ]] && x=$((x-67108864))
+	[[ $y -gt 2047 ]] && y=$((y-4095))
+	[[ $z -gt 33554431 ]] && z=$((z-67108864))
+}
+
+encode_position() {
+	local x
+	local y
+	local z
+
+	x=$1
+	y=$2
+	z=$3
+
+	[[ $x -lt 33554433 ]] && x=$((x+67108864))
+	[[ $y -lt 2049 ]] && y=$((y+4095))
+	[[ $z -lt 33554433 ]] && z=$((z+67108864))
+	
+	printf "%016x" $((((x & 0x3FFFFFF)<<38) | ((z & 0x3FFFFFF)<<12) | (y & 0xFFF)))
 }
